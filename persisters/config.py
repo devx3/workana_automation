4b1ac4f8b7ee5abda44ef6ca10ff1cd213f094cb
DevX3 <erickfabiani123@gmail.com>
2020-08-25 08:13:39 -0300

    refactor: Alterado arquivo de exemplo de configuração

---
+++ persisters/config.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import json
+
+
+class Config(object):
+
+    CONFIG_FILE = os.path.join(os.getcwd(), 'config.json')
+
+    def up(self):
+        ''' Start up the class '''
+        self.create_file()
+
+        with open(self.CONFIG_FILE) as config_file:
+            self.config = json.load(config_file)
+
+    def get(self, attribute):
+        ''' Get the Configuration Value '''
+        assert isinstance(attribute, str), "Attribute must be a string"
+        assert '.' in attribute, "Attribute must be separated by dot"
+
+        section, configuration = attribute.split('.')
+
+        return self.config['config'][section][configuration]
+
+    def file_exists(self):
+        ''' Check if config file exists '''
+        return os.path.exists(self.CONFIG_FILE)
+
+    def create_file(self):
+        ''' Create the config file if NOT exists '''
+        if not self.file_exists():
+            config_example = self.CONFIG_FILE+'.example'
+            shutil.copyfile(config_example, self.CONFIG_FILE)
---
+++ tests/test_config_persister.py
@@ -0,0 +1,56 @@
+try:
+    import sys
+    import os
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                '..'
+            )
+        )
+    )
+
+except Exception:
+    pass
+
+from persisters.config import Config
+import unittest
+import json
+
+
+class TestConfigPersister(unittest.TestCase):
+    def setUp(self):
+        self.config = Config()
+
+    def test_create_file_if_not_exists(self):
+        self.config.create_file()
+
+    def test_if_config_file_exists(self):
+        self.assertTrue(self.config.file_exists())
+
+    def test_attribute_of_get_must_be_an_string(self):
+        with self.assertRaises(AssertionError):
+            self.config.get(1234)
+
+    def test_attribute_of_get_need_to_have_dot(self):
+        with self.assertRaises(AssertionError):
+            self.config.get('connections')
+
+    def test_get_config_total_connections(self):
+        self.config.up()
+        self.assertEqual(self.config.get('connections.total'), 5)
+
+    def test_get_config_current_connections(self):
+        self.config.up()
+        self.assertEqual(self.config.get('connections.current'), 5)
+
+    def test_get_config_times_a_day(self):
+        self.config.up()
+        self.assertEqual(self.config.get('connections.times_a_day'), 3)
+
+    def test_save_num_proposes_of_the_day(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
